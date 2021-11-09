3b70f9a496658ec7ba79c57d2b58acc60c0472b1
jddadev <joethebartenderpdx@gmail.com>
2021-11-09 12:57:12 -0800

    logo finished, search bar started

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,25 @@
+import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/solid';
+
 function Header() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      <div>
+        <input placeholder="Start your search" />
+      </div>
+
+      <div>Left</div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;

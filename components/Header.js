ca726373a100cb4fb1c012cc26be339cb8baad3f
jddadev <joethebartenderpdx@gmail.com>
2021-11-09 22:42:24 -0800

    Header component complete

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/solid';
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from '@heroicons/react/solid';
 
 function Header() {
   return (
@@ -15,13 +21,21 @@ function Header() {
 
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
-          className="flex-grow pl-5 bg-transparent outline-none"
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           placeholder="Start your search"
         />
-        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 rounded-full text-white p-2 cursor-pointer" />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 rounded-full text-white p-2 cursor-pointer md:mx-2" />
       </div>
 
-      <div>Left</div>
+      <div className="flex items-center justify-end  text-gray-500 space-x-4">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
+        </div>
+      </div>
     </header>
   );
 }

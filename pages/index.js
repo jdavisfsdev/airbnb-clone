170a3a79966422bdb5f03673d8ff8671a93fffeb
jddadev <joethebartenderpdx@gmail.com>
2021-11-09 23:27:51 -0800

    SmallCards created/finished

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import SmallCards from '../components/SmallCards';
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -13,6 +14,35 @@ export default function Home() {
       <Header />
 
       <Banner />
+
+      <main className="max-w-7xl mx-aut px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+            {exploreData?.map(({ location, distance, img }) => (
+              <SmallCards
+                key={location}
+                location={location}
+                distance={distance}
+                img={img}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }

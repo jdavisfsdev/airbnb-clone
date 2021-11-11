84d58f477ba31a0308c6c6b4893d28c17d11fb49
jddadev <joethebartenderpdx@gmail.com>
2021-11-10 18:21:38 -0800

    setting num of guests to state, not complete

--- components/Header.js
+++ components/Header.js
@@ -15,6 +15,7 @@ function Header() {
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [numOfGuests, setNumOfGuests] = useState(1);
 
   const selectionRange = {
     startDate,
@@ -59,13 +60,24 @@ function Header() {
       </div>
 
       {searchInput && (
-        <div>
+        <div className="flex flex-col col-span-3 mx-auto">
           <DateRangePicker
             ranges={[selectionRange]}
             minDate={new Date()}
             rangeColors={['#FD5B61']}
             onChange={handleSelect}
           />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl font-semibold flex-grow">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+              placeholder="1"
+            />
+          </div>
         </div>
       )}
     </header>

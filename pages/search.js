fff1e6cd7babe9b1b6787551ba45e82c9f30a977
jddadev <joethebartenderpdx@gmail.com>
2021-11-12 11:52:13 -0800

    pull query params using useRouter hook

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,29 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useRouter } from 'next/dist/client/router';
+import { format } from 'date-fns';
 
 function Search() {
+  // useRouter includes a router.query selector
+  const router = useRouter();
+  //   ]destructuring the URL queries, no need to type router.query.location etc.
+  const { location, startDate, endDate, numOfGuests } = router.query;
+  const formattedStartDate = format(new Date(startDate), 'dd MMMM yy');
+  const formattedEndDate = format(new Date(endDate), 'dd MMMM yy');
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
   return (
     <div>
       <Header />
 
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">300+ Stays for 5 number of guests</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in CITY</h1>
+          <p className="text-xs">
+            300+ Stays from {range} for {numOfGuests} guests.
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}.
+          </h1>
 
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>

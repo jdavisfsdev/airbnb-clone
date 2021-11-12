1624700dc8bcec5a8523a545db5b4d3907829801
jddadev <joethebartenderpdx@gmail.com>
2021-11-12 11:57:31 -0800

    pulled in query params for searchplaceholer using useRouter

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ import { DateRangePicker } from 'react-date-range';
 import { useState } from 'react';
 import { useRouter } from 'next/dist/client/router';
 
-function Header() {
+function Header({ placeholder }) {
   const [searchInput, setSearchInput] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
@@ -66,7 +66,7 @@ function Header() {
           onChange={(e) => setSearchInput(e.target.value)}
           value={searchInput}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
-          placeholder="Start your search"
+          placeholder={placeholder || 'Start Your Search.'}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 rounded-full text-white p-2 cursor-pointer md:mx-2" />
       </div>

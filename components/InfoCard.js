5b623fc4398352d86ebf7329771d8f47bcdee70e
jddadev <joethebartenderpdx@gmail.com>
2021-11-12 23:26:59 -0800

    Styled out InfoCard a little more

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,8 +1,10 @@
 import Image from 'next/image';
+import { HeartIcon } from '@heroicons/react/outline';
+import { StartIcon } from '@heroicons/react/solid';
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div>
+    <div className="flex">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
         <Image
           className="rounded-lg"
@@ -11,7 +13,16 @@ function InfoCard({ img, location, title, description, star, price, total }) {
           objectFit="cover"
         />
       </div>
-      <div></div>
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="h-7 cursor-pointer" />
+        </div>
+
+        <h4 className="text-xl">{title}</h4>
+
+        <div className="border-b w-10 pt-2" />
+      </div>
       <div></div>
       <div></div>
     </div>

a8edd096b988cafc5fa33a8d650c85045a98ded8
jddadev <joethebartenderpdx@gmail.com>
2021-11-15 22:36:53 -0800

    InfoCard styled out, loading bar implemented

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import { HeartIcon } from '@heroicons/react/outline';
-import { StartIcon } from '@heroicons/react/solid';
+import { StarIcon } from '@heroicons/react/solid';
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div className="flex">
+    <div className="flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
         <Image
-          className="rounded-lg"
+          className="rounded-2xl"
           src={img}
           layout="fill"
           objectFit="cover"
@@ -22,6 +22,19 @@ function InfoCard({ img, location, title, description, star, price, total }) {
         <h4 className="text-xl">{title}</h4>
 
         <div className="border-b w-10 pt-2" />
+
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+
+        <div className="flex justify-between items-end pt-5">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {star}
+          </p>
+          <div>
+            <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
       </div>
       <div></div>
       <div></div>

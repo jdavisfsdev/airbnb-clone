7cede2fc48dbc2669e4579e54fcf250fad8b8521
jddadev <joethebartenderpdx@gmail.com>
2021-11-29 00:00:03 -0800

    centered map, displayed emoji for each location

--- components/Map.js
+++ components/Map.js
@@ -1,21 +1,52 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { getCenter } from 'geolib';
 
-function Map() {
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+  //   Munge data to fit the shape getCenter requires
+  //    {latitude: 21.254685, longitude: 14.55484}
+  const coords = searchResults.map((item) => ({
+    latitude: item.lat,
+    longitude: item.long,
+  }));
+
+  const center = getCenter(coords);
+
   const [viewport, setViewport] = useState({
     width: '100%',
     height: '100%',
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: center.latitude,
+    longitude: center.longitude,
     zoom: 11,
   });
+  console.log(selectedLocation);
 
   return (
     <ReactMapGL
       mapStyle="mapbox://styles/jdpdx1/ckw1qk7koa19s15qp011muxdy"
       mapboxApiAccessToken={process.env.mapbox_key}
       {...viewport}
-    ></ReactMapGL>
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {searchResults?.map((item) => (
+        <div key={item.long}>
+          <Marker
+            longitude={item.long}
+            latitude={item.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              onClick={() => setSelectedLocation(item)}
+              className="cursor-pointer text-2xl animate-bounce"
+            >
+              🔥
+            </p>
+          </Marker>
+        </div>
+      ))}
+    </ReactMapGL>
   );
 }
 
